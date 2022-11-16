c09b305dfb42049d321203c5fc0b425f49bb03c0
Rawle Becerril <rawlesxm@gmail.com>
2022-11-15 22:35:10 -0500

    finished coffee app

---
+++ models/coffee.js
@@ -0,0 +1,32 @@
+const coffees = [
+    {
+        "name": "Caramel Frapp",
+        "price": 35,
+        "description": "Super caramelly Frozen Drink",
+        "hasCream": true
+    },
+
+    {
+        "name": "Mocha Frapp",
+        "price": 30,
+        "description": "Super Mocha Frozen Drink",
+        "hasCream": false
+    } ,
+
+    {
+        "name": "Hot Chocolate",
+        "price": 3,
+        "description": "Super chocolately Hot Drink",
+        "hasCream": true
+    }, 
+
+    {
+        "name": "Soy Latte",
+        "price": 350,
+        "description": "Super soy Frozen Drink",
+        "hasCream": false
+    }
+
+]
+
+module.exports = coffees
---
+++ server.js
@@ -0,0 +1,53 @@
+const express = require('express')
+const dotenv = require('dotenv') 
+const app = express()
+dotenv.config()
+const PORT = process.env.PORT
+let coffees = require('./models/coffee.js')
+console.log(coffees)
+
+//Middleware
+app.use(express.urlencoded({extended: true}))
+
+//lets controllers know how to look in view folder to ejs files
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+    res.send(`What's up doc?`)
+})
+
+app.get('/coffees', (req, res) => {
+    res.render('coffee_index.ejs', {
+       allCoffees: coffees
+    })
+})
+
+
+app.get('/coffees/new', (req, res) => {
+    res.render("newdrink.ejs")
+})
+
+
+app.get('/coffees/:index', (req, res) => {
+    res.render('coffee_show.ejs', {
+        drink: coffees[req.params.index],
+        index: req.params.index
+    })
+})
+
+app.post('/coffees', (req, res) => {
+    req.body.price = parseInt(req.body.price)
+    if (req.body.hasCream === "on"){
+        req.body.hasCream = true;
+    } else {
+        req.body.hasCream = false
+    }
+    coffees.push(req.body)
+    console.log(req.body)
+    res.redirect('/coffees')
+})
+
+
+app.listen(PORT, () => {
+    console.log(`Can you feel the ${PORT} tonightttt?`)
+}) 
